695a1720cdfcfa59c4161532382c99f1f09ab051
Igor Mironchik <igor.mironchik@gmail.com>
2024-09-16 10:45:33 +0300

    Add missing files.

---
+++ tests/auto/test_build/multiple_definitions/1.cpp
@@ -0,0 +1,13 @@
+
+/*
+	SPDX-FileCopyrightText: 2022-2024 Igor Mironchik <igor.mironchik@gmail.com>
+	SPDX-License-Identifier: MIT
+*/
+
+#include "1.hpp"
+
+void
+A::parse()
+{
+	MD::Parser< TRAIT > parser;
+}
---
+++ tests/auto/test_build/multiple_definitions/2.cpp
@@ -0,0 +1,13 @@
+
+/*
+	SPDX-FileCopyrightText: 2022-2024 Igor Mironchik <igor.mironchik@gmail.com>
+	SPDX-License-Identifier: MIT
+*/
+
+#include "2.hpp"
+
+void
+B::parse()
+{
+	MD::Parser< TRAIT > parser;
+}
